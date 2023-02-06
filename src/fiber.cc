2dfb7d66e96b86c017c180ee9be9c39ee49fbe42
rleavrs <rleavrs@outlook.com>
2023-02-06 22:00:39 +0800

    thread

---
+++ src/fiber.cc
@@ -0,0 +1,92 @@
+#include "fiber.h"
+#include <atomic>
+
+namespace rleavrs {
+
+static std::atomic<uint64_t> s_fiber_id {0};
+static std::atomic<uint64_t> s_fiber_count {0};
+
+static thread_local Fiber* t_fiber = nullptr;
+static thread_local Fiber::ptr t_threadFiber = nullptr;
+
+
+Fiber::Fiber(std::function<void()> cb, size_t stacksize, bool use_caller) {
+
+}
+
+Fiber::~Fiber() {
+
+}
+
+void Fiber::reset() {
+
+}
+void Fiber::swapIn() {
+
+    
+
+}
+void Fiber::swapOut() {
+
+}
+void Fiber::call() {
+
+}
+void Fiber::back() {
+
+}
+uint64_t Fiber::getId() const {
+    return m_id;
+}
+Fiber::State Fiber::getState() const {
+    return m_state;
+}
+void Fiber::SetThis(Fiber* f) {
+    t_fiber = f;
+}
+Fiber::ptr Fiber::GetThis() {
+    return t_threadFiber;
+}
+
+uint64_t Fiber::GetFiberId() {
+    return s_fiber_id;
+}
+
+void Fiber::YieldToReady() {
+
+}
+void Fiber::YieldToHold() {
+
+}
+uint64_t Fiber::TotalFibers() {
+
+}
+void Fiber::MainFunc() {
+    Fiber::ptr cur = GetThis();
+    
+    cur->m_cb();
+    cur->m_cb = nullptr;
+    cur->m_state = TERM;
+    
+    auto raw_ptr = cur.get();
+    cur.reset();
+    raw_ptr->swapOut();
+    
+
+}
+void Fiber::CallerMainFunc() {
+    Fiber::ptr cur = GetThis();
+    
+    cur->m_cb();
+    cur->m_cb = nullptr;
+    cur->m_stack = TERM;
+
+    auto raw_ptr = cur.get();
+    cur.reset();
+    raw_ptr->back();
+
+}
+
+
+
+}
---
+++ src/fiber.h
@@ -0,0 +1,61 @@
+#ifndef __RLEAVRS__FIBER_H__
+#define __RLEAVRS__FIBER_H__
+
+#include <stdint.h>
+#include <unistd.h>
+#include <memory>
+#include <ucontext.h>
+#include <functional>
+
+namespace rleavrs {
+
+class Sechduler;
+
+class Fiber {
+public:
+    typedef std::shared_ptr<Fiber> ptr;
+
+    enum State {
+        INIT,
+        HOLD,
+        EXEC,
+        TERM,
+        READY,
+        EXCEPT
+    };
+
+private:
+    Fiber();
+
+public:
+    Fiber(std::function<void()> cb, size_t stacksize = 0, bool use_caller = false);
+    ~Fiber();
+    void reset();
+    void swapIn();
+    void swapOut();
+    void call();
+    void back();
+    uint64_t getId() const;
+    State getState() const;
+    
+    static void SetThis(Fiber* f);
+    static Fiber::ptr GetThis();
+    static void YieldToReady();
+    static void YieldToHold();
+    static uint64_t TotalFibers();
+    static void MainFunc();
+    static void CallerMainFunc();
+    static uint64_t GetFiberId();
+
+private:
+    uint64_t m_id = 0;
+    uint32_t m_stacksize = 0;
+    State m_state = State::INIT;
+    ucontext_t m_ctx;
+    void* m_stack = nullptr;
+    std::function<void()> m_cb;
+};
+
+}
+
+#endif
