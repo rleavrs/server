c4028c2439222baadb5d54756e6c174b66cd3609
rleavrs <rleavrs@outlook.com>
2023-02-08 17:24:19 +0800

    fiber

--- src/fiber.cc
+++ src/fiber.cc
@@ -5,6 +5,8 @@
 
 namespace rleavrs {
 
+static Logger::ptr g_logger = RLEAVRS_LOG_NAME("system");
+    
 static std::atomic<uint64_t> s_fiber_id {0};
 static std::atomic<uint64_t> s_fiber_count {0};
 
@@ -28,10 +30,11 @@ Fiber::Fiber() {
     SetThis(this);
 
     if(getcontext(&m_ctx)) {
-        
+        RLEAVRS_ASSERT_W(false, "getcontext");
     }
-
+    
     ++s_fiber_count;
+    RLEAVRS_LOG_DEBUG(g_logger) << "Fiber::Fiber main";
 }
 
 
@@ -39,10 +42,8 @@ Fiber::Fiber(std::function<void()> cb, size_t stacksize, bool use_caller)
     :m_id(++s_fiber_id),
     m_cb(cb) {
     ++s_fiber_count;
-    m_stacksize = stacksize ? stacksize : 1024;
-    
-    m_stack = MallocStackAllocator::Alloc(1024);
-    // alloc
+    m_stacksize = stacksize ? stacksize : 128 * 1024; // cooperating with config system, but now use default value(128 * 1024 = 128M)
+    m_stack = MallocStackAllocator::Alloc(m_stacksize);
     
     if(getcontext(&m_ctx)) {
         RLEAVRS_ASSERT_W (false, "getcontext");
@@ -51,11 +52,28 @@ Fiber::Fiber(std::function<void()> cb, size_t stacksize, bool use_caller)
     m_ctx.uc_link = nullptr;
     m_ctx.uc_stack.ss_sp = m_stack;
     m_ctx.uc_stack.ss_size = m_stacksize;
-    
+
+    if(!use_caller) {
+        makecontext(&m_ctx, &Fiber::MainFunc, 0);
+    } else {
+        makecontext(&m_ctx,&Fiber::CallerMainFunc,0);
+    }
 }
 
 Fiber::~Fiber() {
-
+    s_fiber_count--;
+    if(m_stack) {
+        RLEAVRS_ASSERT( m_state == TERM ||
+                        m_state == INIT ||
+                        m_state == EXCEPT);
+        MallocStackAllocator::Dealloc(m_stack);
+    } else {
+        RLEAVRS_ASSERT(!m_cb);
+        RLEAVRS_ASSERT(m_state == EXEC);
+        Fiber* cur = t_fiber;
+        if(cur == this) SetThis(nullptr);
+    }
+    RLEAVRS_LOG_DEBUG(g_logger) << "FIber::~Fiber id = " << m_id << " tatal = " << s_fiber_count;  
 }
 
 void Fiber::reset(std::function<void()> cb) {
@@ -77,23 +95,23 @@ void Fiber::reset(std::function<void()> cb) {
 
 }
 
-void Fiber::swapIn() {
+void Fiber::fiberSwapIn() {
     // SetThis(this);
     // RLEAVRS_ASSERT(m_state != EXEC);
     // m_state = EXEC;
-    // if(swapcontext(&Sechduler::GetMainFIber))
+    
 }
-void Fiber::swapOut() {
+void Fiber::fiberSwapOut() {
 
 }
-void Fiber::call() {
+void Fiber::threadCall() {
     SetThis(this);
     m_state = EXEC;
     if(swapcontext(&t_threadFiber->m_ctx,&m_ctx)) {
         RLEAVRS_ASSERT_W(false, "swapcontext");
     }
 }
-void Fiber::back() {
+void Fiber::threadBack() {
     SetThis(t_threadFiber.get());
     if(swapcontext(&m_ctx, &t_threadFiber->m_ctx)) {
         RLEAVRS_ASSERT_W(false, "swapcontext");
@@ -113,8 +131,14 @@ void Fiber::SetThis(Fiber* f) {
 }
 
 Fiber::ptr Fiber::GetThis() {
-    return t_threadFiber;
-}
+    if(t_fiber) {
+        return t_fiber->shared_from_this();
+    }
+    Fiber::ptr main_fiber(new Fiber);
+    RLEAVRS_ASSERT(t_fiber == main_fiber.get());
+    t_threadFiber = main_fiber;
+    return t_fiber->shared_from_this();
+}   
 
 uint64_t Fiber::GetFiberId() {
     return s_fiber_id;
@@ -124,14 +148,14 @@ void Fiber::YieldToReady() {
     Fiber::ptr cur = GetThis();
     RLEAVRS_ASSERT(cur->m_state == EXEC);
     cur -> m_state = READY;
-    cur -> swapOut();
+    cur -> fiberSwapOut();
 }
 
 void Fiber::YieldToHold() {
     Fiber::ptr cur = GetThis();
     RLEAVRS_ASSERT(cur->m_state == EXEC);
     cur -> m_state = HOLD;
-    cur -> swapOut();    
+    cur -> fiberSwapOut();
 }
 
 uint64_t Fiber::TotalFibers() {
@@ -147,7 +171,7 @@ void Fiber::MainFunc() {
     
     auto raw_ptr = cur.get();
     cur.reset();
-    raw_ptr->swapOut();
+    raw_ptr->fiberSwapOut();
 }
 
 void Fiber::CallerMainFunc() {
@@ -159,7 +183,7 @@ void Fiber::CallerMainFunc() {
 
     auto raw_ptr = cur.get();
     cur.reset();
-    raw_ptr->back();
+    raw_ptr->threadBack();
 
 }
 
--- src/fiber.h
+++ src/fiber.h
@@ -7,12 +7,20 @@
 #include <ucontext.h>
 #include <functional>
 
+
+// Fiber 
+// makecontext:初始化一个ucontext_t,func参数指明了该context的入口函数，argc为入口参数的个数，每个参数的类型必须是int类型。另外在makecontext之前，一般需要显示的初始化栈信息以及信号掩码集同时也需要初始化uc_link，以便程序退出上下文后继续执行。
+// swapcontext:原子操作，该函数的工作是保存当前上下文并将上下文切换到新的上下文运行。
+// getcontext:将当前的执行上下文保存在ucp中，以便后续恢复上下文
+// setcontext : 将当前程序切换到新的context,在执行正确的情况下该函数直接切换到新的执行状态，不会返回。
+
 namespace rleavrs {
 
 class Sechduler;
 
-class Fiber {
+class Fiber : public std::enable_shared_from_this<Fiber>{
 public:
+    friend Sechduler;
     typedef std::shared_ptr<Fiber> ptr;
 
     enum State {
@@ -31,10 +39,10 @@ public:
     Fiber(std::function<void()> cb, size_t stacksize = 0, bool use_caller = false);
     ~Fiber();
     void reset(std::function<void()> cb);
-    void swapIn();
-    void swapOut();
-    void call();
-    void back();
+    void fiberSwapIn();
+    void fiberSwapOut();
+    void threadCall();
+    void threadBack();
     uint64_t getId() const;
     State getState() const;
     
